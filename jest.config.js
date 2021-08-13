86b0aa2cd8527a269f3f4c4f1747f9744ea03dcd
benNie <bennie@openrice.com>
2021-08-13 09:22:09 +0800

    将单元测试文件类型改为 ts，且能测试 vue3 组件

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.tsx$': 'ts-jest',
+    '^.+\\.tsx?$': 'ts-jest',
   },
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   moduleNameMapper: {
--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
   test('renders props.msg when passed', () => {
