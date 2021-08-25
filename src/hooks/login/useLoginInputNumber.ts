bc47dbca1a6ea748ae51a7db0b2933714560c828
NieHang <1076883711@qq.com>
2021-08-25 18:00:56 +0800

    使用 storage 缓存手机号前缀数据

--- src/hooks/login/useLoginInputNumber.ts
+++ src/hooks/login/useLoginInputNumber.ts
@@ -1,6 +1,10 @@
 import { reactive } from 'vue'
-import { LoginInputNumberStateType } from '@/types/components'
+import {
+  LoginInputNumberStateType,
+  PHONE_NUMBER_PERFIX,
+} from '@/types/components'
 import { apis } from '@/http/backend'
+import storage from 'good-storage'
 
 export default function useLoginInputNumber() {
   const state: LoginInputNumberStateType = reactive({
@@ -11,10 +15,16 @@ export default function useLoginInputNumber() {
     indexList: [],
   })
   const getCountryNumberPerfixAndIndexListData = async () => {
-    apis.loginApis.getCountryNumberPerfix().then((response) => {
-      state.countryNumberPerfix = response.data
-      for (const item in response.data) state.indexList.push(item)
-    })
+    const data = storage.get(PHONE_NUMBER_PERFIX)
+    if (!data) {
+      await apis.loginApis.getCountryNumberPerfix().then((response) => {
+        storage.set(PHONE_NUMBER_PERFIX, response.data)
+      })
+    }
+
+    state.countryNumberPerfix = storage.get(PHONE_NUMBER_PERFIX)
+    for (const item in storage.get(PHONE_NUMBER_PERFIX))
+      state.indexList.push(item)
   }
   const getCurrentNumberPerfix = (event: any) => {
     let parentNode: HTMLElement = event.target.parentNode
--- src/types/components/index.ts
+++ src/types/components/index.ts
@@ -1,10 +1,11 @@
+export const PHONE_NUMBER_PERFIX: string = 'PHONE_NUMBER_PERFIX'
+
 export interface LeftMenuStateType {
   leftMenuAsideRef: null | HTMLElement
   showLoginSection: Boolean
   themeChecked: Boolean
   showLeftMenu: Boolean
 }
-
 export interface LoginInputNumberStateType {
   phoneNumber: null
   showIndexNumberPerfix: Boolean
