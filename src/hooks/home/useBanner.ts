d723e2f0839e49b69a7997ab7b1ed00362e87617
NieHang <1076883711@qq.com>
2021-10-08 16:29:44 +0800

    将 useHome 改名为 useBanner，使其功能更明确且单一，提高可维护性

---
+++ src/hooks/home/useBanner.ts
@@ -0,0 +1,24 @@
+import { apis } from '@/http/backend'
+import { ref } from '@vue/runtime-dom'
+
+export default function useBanner(
+  bannerType: 'Pc' | 'Android' | 'Iphone' | 'Ipad'
+) {
+  let banners = ref(null)
+
+  enum BannerType {
+    Pc = 0,
+    Android,
+    Iphone,
+    Ipad,
+  }
+
+  const getBanners = async (type: number) => {
+    const res = await apis.homeApis.getBanners({ type })
+    banners.value = res.data.banners
+  }
+
+  getBanners(BannerType[bannerType])
+
+  return banners
+}
