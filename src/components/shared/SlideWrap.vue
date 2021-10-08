eb94b0674cea3bf428020a56070a878ba23baa53
NieHang <1076883711@qq.com>
2021-10-08 16:33:05 +0800

    创建滑动公共组件且应用于首页中 feature-slide 部分

---
+++ src/components/shared/SlideWrap.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="slide-wrap">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlideWrap',
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.slide-wrap
+  white-space nowrap
+  overflow overlay
+  scrollbar-width none /* firefox */
+  -ms-overflow-style none /* IE 10+ */
+  &::-webkit-scrollbar
+    display none /* Chrome Safari */
+</style>
